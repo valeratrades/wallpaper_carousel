effd264d98deb96da187b5de219c64abc03c1d86
Valera <valeratrades@gmail.com>
2025-11-20 07:05:58 +0000

    chore: nested components

--- examples/config.nix
+++ examples/config.nix
@@ -1,10 +1,10 @@
 {
   quotes = [
-    "EV compounds,\nembarrassment doesn't"
+    { text = "EV compounds,\nembarrassment doesn't"; author = "Beny"; }
 
     { text = "iteration speed"; author = "Alex Hormozi"; }
 
-    "fuck your mood\nfollow the plan"
+    { text = "fuck your mood\nfollow the plan"; author = "Leila Hormozi"; }
   ];
   balance = {
     command = "rm_engine balance";
